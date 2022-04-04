f738694934ab03d5335053f360f15bffe13530aa
Arnaud Rocher <arnaud.roche3@gmail.com>
2022-04-04 08:02:55 +0000

    fix: use external library to find appropriate locale

--- src/boot/select-locale.ts
+++ src/boot/select-locale.ts
@@ -1,3 +1,4 @@
+import { pickLocale } from "locale-matcher"
 import { Quasar } from "quasar"
 import langPacksIndex from "quasar/lang/index.json"
 import { boot } from "quasar/wrappers"
@@ -6,18 +7,18 @@ export default boot(() => {
   const setLocale = () => {
     const { languages } = navigator
     if (!Array.isArray(languages)) return
-    for (const lang of languages) {
-      if (langPacksIndex.findIndex(({ isoName }) => isoName === lang) !== -1) {
-        const langPacks = import.meta.glob(
-          "../../node_modules/quasar/lang/*.mjs"
-        )
-        langPacks[`../../node_modules/quasar/lang/${lang}.mjs`]().then(
-          (langPkg) => {
-            Quasar.lang.set(langPkg.default)
-          }
-        )
-        return
-      }
+
+    const quasarLocale = pickLocale(
+      languages,
+      langPacksIndex.map((lang) => lang.isoName)
+    )
+    if (quasarLocale !== undefined) {
+      const langPacks = import.meta.glob("../../node_modules/quasar/lang/*.mjs")
+      langPacks[`../../node_modules/quasar/lang/${quasarLocale}.mjs`]().then(
+        (langPkg) => {
+          Quasar.lang.set(langPkg.default)
+        }
+      )
     }
   }
 
