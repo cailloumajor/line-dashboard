a2f76d140b5864dcdd4ec0755e72db01728c53ef
Arnaud Rocher <arnaud.roche3@gmail.com>
2022-11-02 10:24:12 +0000

    chore: remove Quasar template files

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,13 +3,6 @@ import type { RouteRecordRaw } from "vue-router"
 const routes: RouteRecordRaw[] = [
   {
     path: "/",
-    name: "home",
-    component: () => import("../layouts/MainLayout.vue"),
-    children: [{ path: "", component: () => import("../pages/IndexPage.vue") }],
-  },
-
-  {
-    path: "/",
     component: () => import("../layouts/LineInterfaceLayout.vue"),
     children: [
       {
