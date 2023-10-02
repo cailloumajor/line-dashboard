a5ea587227420dbf88ad9d6fe2c567a87f2c6b89
Arnaud Rocher <arnaud.roche3@gmail.com>
2023-10-02 09:47:04 +0000

    chore: remove unused state data

--- src/stores/campaign-data.ts
+++ src/stores/campaign-data.ts
@@ -3,7 +3,6 @@ import { defineStore } from "pinia"
 export const useCampaignDataStore = defineStore("campaign-data", {
   state: () => ({
     currentCampaign: "?",
-    dataValidForCampaign: "!INVALID!",
     targetCycleTime: 0,
     targetEfficiency: 0,
   }),
